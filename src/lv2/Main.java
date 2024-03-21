4ff4197fcce58595508f857c1640b52785a61a51
HBLEEEEE <lhbs0528@gmail.com>
2024-03-22 00:18:57 +0900

    프로그래머스 lv2 가장 큰 정사각형 찾기

--- src/lv2/Main.java
+++ src/lv2/Main.java
@@ -5056,8 +5056,29 @@ class Solution_0080_1 {
     }
 }
 
+//문제 : 가장 큰 정사각형 찾기
+//url : https://school.programmers.co.kr/learn/courses/30/lessons/12905
+class Solution_0081 {
+    public int solution(int[][] board) {
+        int answer = 0;
 
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] > 0) {
+                    int up = (j - 1 >= 0) ? board[i][j - 1] : 0;
+                    int down = (i - 1 >= 0) ? board[i - 1][j] : 0;
+                    int diagonal = (i - 1 >= 0 && j - 1 >= 0) ? board[i - 1][j - 1] : 0;
 
+                    board[i][j] = Math.min(Math.min(up, down), diagonal) + 1;
+                    answer = Math.max(board[i][j], answer);
+                }
+            }
+
+        }
+
+        return answer * answer;
+    }
+}
 
 
 
